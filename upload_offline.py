3a5babd29ef3fa7c2ee3e797f0f284455331b3c4
Elizabeth Won <138541741+elizabethjhwon@users.noreply.github.com>
2023-11-12 11:47:45 +0800

    Update upload_offline.py

--- upload_offline.py
+++ upload_offline.py
@@ -1,15 +1,19 @@
+# Import necessary modules
 import os, json
 from caltechdata_api import caltechdata_edit
 
 # Get access token for CaltechDATA as environment variable with source token.bash
 token = os.environ["RDMTOK"]
 
+# Open and load metadata from a Datacite JSON file
 metaf = open("cd_metadata.json", "r")
 metadata = json.load(metaf)
 
-production = True
-parent = "w6hhb-mc015"
+# Set additional parameters
+production = True # Indicates that changes will be made in the production environment
+parent = "w6hhb-mc015" # Parent dataset or collection ID
 
+# Call the caltechdata_edit function to edit metadata and publish datasets
 response = caltechdata_edit(
     parent,
     metadata,
